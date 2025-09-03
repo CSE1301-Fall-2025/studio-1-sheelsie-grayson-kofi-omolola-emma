43be1633145eea804fe1e2b87cae1d058062c0e3
Grayson <154711928+GraysonHollingsworth@users.noreply.github.com>
2025-09-03 15:18:22 -0500

    Ordered Done

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,7 +15,8 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
-
+		boolean isOrdered = (x <= z) && (y <= z) || (x >= y) && (y >= z);
+		System.out.println(isOrdered);
 	}
 
 }
